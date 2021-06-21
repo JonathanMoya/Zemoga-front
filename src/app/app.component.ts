5ab3a1d2e74e6f2c0ca59f7f1f2f16e0db3293b9
Jonathan Moya <jonmoy@bancolombia.com.co>
2021-06-20 23:43:26 -0500

    card terminated design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as dataJson from './data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PruebaFront';
+  data = dataJson.default.data;
+
+  ngOnInit(): void {
+    console.log(dataJson.default.data);
+  }
 }
