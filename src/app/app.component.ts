25e1cc8dc2bc0bb58841305c7ce5cc993399fe8c
Jonathan Moya <jonmoy@bancolombia.com.co>
2021-06-22 01:21:31 -0500

    Terminado front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,36 +8,10 @@ import * as dataJson from './data.json';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'PruebaFront';
   data = dataJson.default.data;
-  styleCard = 'Grid';
-  thumbSelect = '';
-  stateVote = false;
+  styleCard = 'List';
+
   ngOnInit(): void {
     console.log(dataJson.default.data);
   }
-
-  getPercentege = (data, type: string) => ((data.votes[type] / (data.votes.positive + data.votes.negative)) * 100).toFixed(1);
-
-  differenceTime(date: Date): string {
-    const diffTime = new Date().getTime() - new Date(date).getTime();
-    const days = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    const month = Math.round(days / 30);
-    if (days < 30) {
-      return days + (days < 2 ? ' day' : 'days');
-    } else if (month < 12) {
-      return month + (month < 2 ? ' month' : ' months');
-    } else {
-      return (month / 12).toFixed(0) + ((month / 12) < 2 ? ' year' : ' years');
-    }
-  }
-
-  votar(): void {
-    if (this.thumbSelect !== '') {
-      console.log(this.personajeData);
-      this.personajeData.votes[this.thumbSelect]++;
-      this.stateVote = true;
-    }
-  }
-
 }
