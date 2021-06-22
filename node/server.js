91f210cc60505e9221a61fb5186d49d75377e126
Jonathan Moya <jmoyac@unal.edu.co>
2021-06-22 03:19:13 -0500

    Clean and comments

--- node/server.js
+++ node/server.js
@@ -1,21 +1,26 @@
+// Imports
 const express = require("express");
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 var cors = require('cors')
+
+// Server config
 app.use(cors())
 app.options('*', cors())
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// Mongo connection
 const url = "mongodb+srv://jonathanmoya:jonathanmoya@cluster0.oh0wz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
 client.connect(err => {
   const collection = client.db("data").collection("data");
-  // perform actions on the collection object
   console.log('BD conectada')
   client.close();
 });
 
+// Functions connection with mongo
 const getAll = (cb) => {
   MongoClient.connect(url, function (err, db) {
     if (err) throw err;
@@ -50,6 +55,7 @@ const updateDataNegative = (id) => {
   });
 }
 
+//rest api
 app.post('/update', function (req, res) {
   if (req.body.type === 'positive') {
     updateDataPositive(req.body.name);
@@ -65,15 +71,7 @@ app.get('/', function (req, res) {
   })
 });
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
-
-
+// App listening
 app.listen(3000, () => {
   console.log("El servidor está inicializado en el puerto 3000");
 });
